c2f4a5c5e7ad9672e037707bd6dd2a2ab742ffd3
leedasom050115 <kiss9892@inha.edu>
2025-05-29 17:29:40 +0900

    과제제출합니다

--- src/components/ColorPage.jsx
+++ src/components/ColorPage.jsx
@@ -1,11 +1,23 @@
 import { useParams } from 'react-router-dom';
 import '../styles/ColorPage.css';
 
+<Route path="/mainpage/:id" element={<mainpage />} />
 function ColorPage() {
 
   return (
-    <div className="color-page">
-    배경 페이지입니다 
+    <div
+     className="color-page"
+     style={{
+        backgroundColor: id,
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        fontSize: '2rem',
+     }}
+     >
+    배경 페이지입니다 : {id}
     </div>
   );
 }
